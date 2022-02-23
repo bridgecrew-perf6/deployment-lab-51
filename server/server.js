7cb779de8fe7ff0617567316024d2e10e43b5e32
Landon Taylor <landon.b.taylor@gmail.com>
2022-02-23 14:21:17 -0700

    added server info

--- server/server.js
+++ server/server.js
@@ -6,8 +6,7 @@ const app = express()
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../main.html'))
 })
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../styles.css'))
+app.use(express.static(path.join(__dirname, '../')))
 })
 
 const port = process.env.PORT || 4005;
