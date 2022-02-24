7d6a27469e7af23e7176ed6996356490bc7d7068
Landon Taylor <landon.b.taylor@gmail.com>
2022-02-24 14:05:31 -0700

    added rollbar

--- server/server.js
+++ server/server.js
@@ -3,6 +3,16 @@ const path = require('path')
 
 const app = express()
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'f6539d27d5e84f86b0ff2d29f719ad9d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../main.html'))
 })
